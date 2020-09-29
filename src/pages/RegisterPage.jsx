45259a8f0ed740408e991448f2f5a2a23cc6adfa
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-29 08:31:18 +0200

    Add help text

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import UserKit from '../data/UserKit';
 
 export default function RegisterPage() {
+  const [isSubmitted, setIsSubmitted] = useState(false)
   const [firstName, setFirstName] = useState("Hassan")
   const [lastName, setLastName] = useState("Mian")
   const [email, setEmail] = useState("mian+test+123@willandskill.se")
@@ -12,10 +13,16 @@ export default function RegisterPage() {
   const userKit = new UserKit()
   function handleCreateUser() {
     userKit.register(firstName, lastName, email, password, companyName, "0")
+    .then(res=> res.json())
+    .then(data => {
+      setIsSubmitted(true)
+    })
   }
 
   return (
     <>
+    {!isSubmitted && (
+      <>
       <h1>Register</h1>
       <p><input placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/></p>
       <p><input placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/></p>
@@ -24,6 +31,12 @@ export default function RegisterPage() {
       <p><input placeholder="Company Name" value={companyName} onChange={(e) => setCompanyName(e.target.value)}/></p>
 
       <button onClick={handleCreateUser}>Create User</button>
+      </>
+    )}
+    {isSubmitted && (
+      <p>Please check your email, {email}, for activation link</p>
+    )}
+      
     </>
   )
 }
