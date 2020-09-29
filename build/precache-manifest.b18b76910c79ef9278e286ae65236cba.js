03c6cbf6398a4248f49e80f6d051e8023ceea4ab
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-29 08:37:06 +0200

    Add first build

---
+++ build/precache-manifest.b18b76910c79ef9278e286ae65236cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30bed00574dfc535452027e4605577b",
+    "url": "/js-lessons-business/index.html"
+  },
+  {
+    "revision": "47d0a3fdf732470ee6cf",
+    "url": "/js-lessons-business/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d42e58a55a3fb344bf22",
+    "url": "/js-lessons-business/static/js/2.d7746836.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-lessons-business/static/js/2.d7746836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d0a3fdf732470ee6cf",
+    "url": "/js-lessons-business/static/js/main.18311dbd.chunk.js"
+  },
+  {
+    "revision": "5514c85ca0d5e146a2c7",
+    "url": "/js-lessons-business/static/js/runtime-main.c377b742.js"
+  }
+]);
